f942a6427abf1e30440d1dc545f3ff57c422697b
xibotao <13739774326@163.com>
2021-01-22 15:37:23 +0800

    getters undefined bug init

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,15 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    testData: {}
   },
   mutations: {
   },
   actions: {
   },
   modules: {
+  },
+  getters: {
+    testData: state => state.testData
   }
 })
